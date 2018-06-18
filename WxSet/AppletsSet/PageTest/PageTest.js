73faf2b40343213b9366c82ad2463ac5b073d434
Kirito <1466335092@qq.com>
2018-06-18 20:08:12 +0800

    完成组件集合的编写

--- WxSet/AppletsSet/PageTest/PageTest.js
+++ WxSet/AppletsSet/PageTest/PageTest.js
@@ -3,6 +3,97 @@ let app = getApp();
 let util = require("../UtilsTest/UtilsTest.js");
 Page({
   data: {
+
+    latitude: 23.099994,
+    longitude: 113.324520,
+    markers: [{
+      id: 1,
+      latitude: 23.099994,
+      longitude: 113.324520,
+      title: 'T.I.T 创意园',
+      iconPath: "",
+      rotate: 0,
+      alpha: 1,
+      width: 20,
+      height: 20,
+      callout: {
+        content: "文本",
+        color: "blue",
+        fontSize: 30,
+        borderRadius: 10,
+        bgColor: "yellow",
+        padding: 5,
+        display: "BYCLICK",
+        textAlign: "center"
+      },
+      label: {
+        content: "label文本",
+        color: "blue",
+        fontSize: 30,
+        anchorX: 23.099998,
+        anchorY: 113.324520,
+        borderWidth: 5,
+        borderColor: "red",
+        borderRadius: 15,
+        bgColor: "green",
+        padding: "18",
+        textAlign: "center",
+      },
+      anchor: {
+        x: 0.5,
+        y: 1
+      }
+    }],
+    polyline: [{
+      points: [{
+        longitude: 23,
+        latitude: 23
+      }, {
+        longitude: 23.1,
+        latitude: 23.1
+      }],
+      color: "#FF0000DD",
+      width: 20,
+      dottedLine: true,
+      arrowLine: true,
+      arrowIconPath: undefined,
+      borderColor: "#fff",
+      borderWidth: 8
+    }],
+    circles: [{
+      latitude: 23.099994,
+      longitude: 113.324520,
+      color: '#FF0000DD',
+      fillColor: '#7cb5ec88',
+      radius: 3000,
+      strokeWidth: 1
+    }],
+    controls: [{
+        id: 1,
+        iconPath: '../images/ImageTest.jpg',
+        position: {
+          left: 180,
+          top: 100 - 50,
+          width: 20,
+          height: 20
+        },
+        clickable: true
+      },
+      {
+        id: 2,
+        iconPath: '../images/ImageTest.jpg',
+        position: {
+          left: 200,
+          top: 100 - 50,
+          width: 20,
+          height: 20
+        },
+        clickable: true
+      }
+    ],
+
+
+
     data_test: "data_test_data",
     data_test_array: [...Array(5).keys()],
     nodes: [{
@@ -14,8 +105,19 @@ Page({
       children: [{
         type: 'text',
         text: 'Hello&nbsp;World!'
-      }]
+      }],
     }],
+    danmuList: [{
+        text: '第 30s 出现的弹幕',
+        color: '#ff0000',
+        time: 30
+      },
+      {
+        text: '第 45s 出现的弹幕',
+        color: '#ff00ff',
+        time: 45
+      }
+    ]
   },
   onLoad: function(opt) {
     console.log("Page onLoad", opt)
