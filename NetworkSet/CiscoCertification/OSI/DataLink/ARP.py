97390e261400bf346fe0138fe5241992c22541f8
Kirito <1466335092@qq.com>
2018-07-28 20:38:17 +0800

    更改知识类库

---
+++ NetworkSet/CiscoCertification/OSI/DataLink/ARP.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+BYTES = 0xFF
+
+
+class ICMPProtocol:
+
+    def __init__(self):
+        self.hardware_type = BYTES * 2
+        self.protocol_type = BYTES * 2
+        self.hardware_size = BYTES * 1
+        self.protocol_size = BYTES * 1
+
+        self.option_code = BYTES * 2
+
+        self.sender_mac_address = BYTES * 6
+        self.sender_ip_address = BYTES * 4
+        self.target_mac_address = BYTES * 6
+        self.target_ip_address = BYTES * 4
---
+++ NetworkSet/CiscoCertification/OSI/Network/ICMP.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+
+BYTES = 0xFF
+
+
+class ICMPProtocol:
+
+    def __init__(self):
+        self.type = BYTES * 1
+        self.code = BYTES * 1
+        self.checksum = BYTES * 2
+        self.flag = BYTES * 2
+
+    def get_type(self, num):
+        if num < self.type:
+            return {
+                0: "回送请求或回答",
+                3: "终点不可达",
+                5: "改变路由",
+                8: "回送请求或回答",
+                11: "时间超过",
+                12: "参数问题",
+                13: "时间戳请求或回答",
+                14: "时间戳请求或回答",
+            }.get(num, None)
