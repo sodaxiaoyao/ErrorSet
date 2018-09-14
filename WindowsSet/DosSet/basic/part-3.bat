303aeaa0924ef4128e64b8928975c0c6a29d63c9
SystemLight <1466335092@qq.com>
2018-09-14 10:59:39 +0800

    增加文件设置的DOS

---
+++ WindowsSet/DosSet/basic/part-3.bat
@@ -0,0 +1,34 @@
+@echo off
+
+:: 复制文件
+copy d:\1.txt c:\
+
+:: 目录下的所有文件
+xcopy /E e:\test e:\
+
+:: 重命名文件
+ren e:\3.txt test.txt
+
+:: 比较文件的差异性
+fc e:\1.txt e:\2.txt
+
+:: 设置文件属性
+attrib /S /D +H +R e:\test
+
+:: 删除文件
+del e:\test
+
+:: 清楚屏幕
+cls
+
+:: 查看dos版本号
+ver
+
+:: 显示或者设置日期
+date
+
+:: 显示或者设置时间
+time
+
+
+pause
