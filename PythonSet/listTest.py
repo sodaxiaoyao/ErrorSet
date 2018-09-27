2b89b2a9193b7317c0aa7ee8dc52c7ab5c6d9a11
SystemLight <1466335092@qq.com>
2018-09-27 21:56:38 +0800

    添加语法测试模块

--- PythonSet/listTest.py
+++ PythonSet/listTest.py
@@ -8,5 +8,27 @@ def _help():
     print(list.__doc__)
 
 
+def _append():
+    # list对象添加元素
+    test_obj.append(1)
+
+
+def _extend():
+    # list对象合并
+    test_obj.extend([1, 2, 3] + [3, 4, 5])
+
+
+def _insert():
+    # list对象插入元素
+    test_obj.insert(2, 0)
+
+
+def _del():
+    # list对象删除元素,切片是一种复制
+    del test_obj[0]
+    test_obj[:-1].remove(0)
+    test_obj.pop()
+
+
 if __name__ == "__main__":
     _help()
---
+++ PythonSet/syntaxTest.py
@@ -0,0 +1,37 @@
+# TYPE: 命名规则
+
+
+def _help():
+    # Python项目命名规则
+    naming_rules = {
+        "项目": "Word_word",
+        "包": "word_word",
+
+        "模块": "wordWord",
+        "类": "WordWord",
+
+        "方法": "do_word",
+        "函数": "do_word",
+
+        "变量": "word_word",
+        "常量": "WORD_WORD",
+
+        "私有": "__word",
+        "冲突": "_word",
+        "魔法": "__word__"
+    }
+    naming_rules.update({"填充符": "fill"})
+
+
+def _bitwise():
+    # 60 = 00111100 ,13 = 00001101 : 位运算符
+    print(bin(60 & 13).replace("0b", "").zfill(8), ":与")
+    print(bin(60 | 13).replace("0b", "").zfill(8), ":或")
+    print(bin(60 ^ 13).replace("0b", "").zfill(8), ":异或")
+    print(bin(~13).replace("0b", "").zfill(8), ":取反")
+    print(bin(13 >> 2).replace("0b", "").zfill(8), ":右移")
+    print(bin(13 << 2).replace("0b", "").zfill(8), ":左移")
+
+
+if __name__ == "__main__":
+    _bitwise()
