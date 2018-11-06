c7e44fbcff8e06a69dc391c04cbddc743dc5dbf1
SystemLight <1466335092@qq.com>
2018-11-06 21:09:28 +0800

    补充新模块

---
+++ PythonSet/a_create_tool.py
@@ -0,0 +1,48 @@
+import os.path
+
+name = input("请输入模块：")
+module_name = name
+name = "{}Test.py".format(name)
+print(name)
+if not os.path.exists(name):
+    with open(name, 'w', encoding="utf8") as fp:
+        the_type = input("输入数字（内置模块1，第三方模块2，内置包3，第三方包4）：")
+        flag = False
+        content = """import re
+
+
+# TYPE: source s_type
+
+def _help():
+    # 交互界面查看文档，使用help(),退出quit
+    print(re.__doc__)
+
+
+def _example():
+    # 例子
+    pass
+
+
+if __name__ == "__main__":
+    # _help()
+    pass
+"""
+
+        if str(the_type) == '1':
+            content = content.replace("source", "Built-in").replace("s_type", "module")
+        elif str(the_type) == '2':
+            content = content.replace("source", "Third-party").replace("s_type", "module")
+        elif str(the_type) == '3':
+            content = content.replace("source", "Built-in").replace("s_type", "package")
+        elif str(the_type) == '4':
+            content = content.replace("source", "Third-party").replace("s_type", "package")
+        else:
+            print("输入错误")
+            flag = True
+
+        if not flag:
+            print(name + "被创建")
+            fp.write(content.replace("re", module_name))
+
+else:
+    print("已经存在")
---
+++ PythonSet/pyinstallerTest.py
@@ -0,0 +1,24 @@
+import PyInstaller
+
+
+# TYPE: Third-party package
+
+def _help():
+    # 交互界面查看文档，使用help(),退出quit
+    print(PyInstaller.__doc__)
+
+
+def _create():
+    # 创建的文件只支持UTF-8编码格式
+    return {
+        "pyinstaller 文件名称": "基本创建",
+        "-h": "查看帮助",
+        "-F": "生成单个可执行文件",
+        "-w": "去掉控制台界面，没有GUI的时候不要使用",
+        "-i='path'": "可执行文件的图标"
+    }
+
+
+if __name__ == "__main__":
+    # _help()
+    pass
